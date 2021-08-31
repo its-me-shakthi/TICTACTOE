e79e6fb89c3e9cddade025ede17db1cf4e90bd0c
its-me-shakthi <87612860+its-me-shakthi@users.noreply.github.com>
2021-08-31 17:11:23 +0530

    Update tic.js

--- tic.js
+++ tic.js
@@ -57,7 +57,7 @@ const getRow = function (a, b, c, move) {
   return result;
 };
 const box = function (number) {
-  return document.getElementById("cel" + number);
+  return document.getElementById("cel" + number).innerText;
 };
 // cell.forEach((cell) => {
 //   cell.addEventListener("click", tabclick);
